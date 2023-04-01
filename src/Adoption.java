a14eae003b36dea62ee5a268f5f3802021b8c792
Manuel2u <edodoo011@st.ug.edu.gh>
2023-04-01 03:10:49 +0000

    added comment

--- src/Adoption.java
+++ src/Adoption.java
@@ -1,17 +1,27 @@
+// Import the ArrayList class
 import java.util.ArrayList;
 
+// Create a class named Adoption that implements the PetStoreSpecification interface
 public class Adoption implements PetStoreSpecification {
 
-
+    // Implement the adoptionDrive method specified in the PetStoreSpecification interface
     @Override
     public void adoptionDrive(ArrayList<Object> pets, double price) {
-
+        // Code for the adoption drive method goes here
     }
 
+    // Create a default constructor for the Adoption class
     public Adoption(){}
 
+    // Implement the inventoryValue method specified in the PetStoreSpecification interface
     @Override
     public double inventoryValue() {
-        return 0;
+        return 0; // The inventory value is currently set to 0, but will be calculated based on the pets in the store
     }
 }
+
+// The above code defines a class named Adoption that implements the PetStoreSpecification interface.
+// The class includes two methods: adoptionDrive and inventoryValue.
+// adoptionDrive takes in an ArrayList of Object pets and a double price, and can be used to run an adoption drive for the pets in the store.
+// inventoryValue returns the current inventory value of the pet store, which is currently set to 0, but will be calculated based on the pets in the store.
+// The class has a default constructor, which does not take any arguments.
